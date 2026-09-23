3046e1cb05f4ce116a4869f90d003add8c07d493
reniyasrayeva <reniyasnadar717@gmail.com>
2026-09-23 15:19:30 +0530

    Add TURN servers to WebRTC

--- app_views/live_detection.py
+++ app_views/live_detection.py
@@ -211,7 +211,14 @@ def render():
             media_stream_constraints={"video": True, "audio": False},
             async_processing=True,
             rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                "iceServers": [
+                    {"urls": ["stun:stun.l.google.com:19302"]},
+                    {
+                        "urls": ["turn:openrelay.metered.ca:80", "turn:openrelay.metered.ca:443", "turn:openrelay.metered.ca:443?transport=tcp"],
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject"
+                    }
+                ]
             }
         )
 
